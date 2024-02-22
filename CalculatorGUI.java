710ea787a551f21566be170d6ce479f670435824
Eeshan-Vaghjiani <101983223+Eeshan-Vaghjiani@users.noreply.github.com>
2024-02-22 17:39:23 +0300

    Equals function

--- CalculatorGUI.java
+++ CalculatorGUI.java
@@ -1,3 +1,7 @@
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Color;
+import java.awt.Canvas;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
@@ -8,6 +12,9 @@ public class CalculatorGUI extends JFrame implements ActionListener {
 
     // Define instance variables
     public String currentText = ""; // Store current input text
+    public double memory;
+    public double value;
+
     public JTextField screen = new JTextField(); // Text field to display input and result
     Calculator calc = new Calculator(); // Calculator instance
 
@@ -17,8 +24,36 @@ public class CalculatorGUI extends JFrame implements ActionListener {
             "2ⁿᵈ", "x²", "x³", " xʸ", "eˣ", "10ˣ", "7", "8", "9", "×",
             "1/x", "²√x", "∛x", "ʸ√ₓ", "ln", "log₁₀", "4", "5", "6", "-",
             "X!", "sin", "cos", "tan", "e", "EE", "1", "2", "3", "+",
-            "Rad", "sin⁻¹", "cos⁻¹", "tan⁻¹", "π", "Rand", "0","0", ".", "=",
+            "Rad", "sin⁻¹", "cos⁻¹", "tan⁻¹","π", "Rand", "0","0", ".", "=",
     };
+    public void clearMemory() {
+        this.memory = 0.0;
+    }
+
+    public double getMemory() {
+        return memory;
+    }
+    public void equals(){
+
+        currentText  = currentText.replace("×", "*");
+        currentText = currentText.replace("÷", "/");
+        double result = calc.eval(currentText);
+        if (result % 1 == 0) {
+            int int_result = (int) result;
+            currentText = int_result + "";
+        } else {
+            currentText = result + "";
+        }
+    }
+    public Object addToMemory() {
+        this.memory += value ;
+        return null;
+    }
+
+    public Object subtractFromMemory() {
+        this.memory -= value;
+        return null;
+    }
 
     // Constructor
     public CalculatorGUI() {
@@ -32,7 +67,7 @@ public class CalculatorGUI extends JFrame implements ActionListener {
         // Initialize and configure the text field
         Font buttonFont = new Font("Arial", Font.PLAIN, 200); // Set font for text field
         screen.setHorizontalAlignment(JTextField.RIGHT); // Set text alignment to right
-        screen.setEditable(false); // Make text field non-editable
+        screen.setEditable(true); // Make text field non-editable
         screen.setPreferredSize(new Dimension(900, 100)); // Increase the height of the screen
         // Modify font size for screen text
         Font screenFont = screen.getFont();
@@ -43,7 +78,6 @@ public class CalculatorGUI extends JFrame implements ActionListener {
         screen.setSize(900, 300); // Set initial size
         add(screen, BorderLayout.NORTH); // Add text field to the frame's north position       screen.setFont(new Font("Arial", Font.BOLD, 20));
         screen.setHorizontalAlignment(JTextField.RIGHT);
-        screen.setEditable(false);
         screen.setBackground(new Color(50,50,47));
         screen.setForeground(new Color(235, 233, 232));
         screen.setBorder(BorderFactory.createMatteBorder(1,1,1,1, new Color(50,50,47)));
@@ -75,7 +109,7 @@ public class CalculatorGUI extends JFrame implements ActionListener {
             // Set background color based on button type
             if (".".equals(label) || label.matches("[0-9 ]")) {
                 button.setBackground(new Color(100, 100, 98)); // Set background color for numbers and decimal point
-            } else if ("÷×-+= ".contains(label)) {
+            } else if ("÷×-+=".contains(label)) {
                 button.setBackground(new Color(255, 159, 9)); // Set background color for operators
             } else {
                 button.setBackground(new Color(69, 68, 66)); // Set background color for other buttons
@@ -94,130 +128,178 @@ public class CalculatorGUI extends JFrame implements ActionListener {
     // ActionListener implementation
     @Override
     public void actionPerformed(ActionEvent e) {
-
         String command = e.getActionCommand(); // Get the action command of the button clicked
-        switch (command) {
-            // Handle different button actions
-            case "=":
-                // Evaluate expression
-                currentText = currentText.replace("×", "*");
-                currentText = currentText.replace("÷", "/");
-                double result = calc.eval(currentText);
-                if (result % 1 == 0) {
-                    int int_result = (int) result;
-                    currentText = int_result + "";
-                } else {
-                    currentText = result + "";
-                }
-                break;
-            case "c":
-                currentText = ""; // Clear current text
-                break;
-            case "e":
-                currentText += Math.E; // Append Euler's number to current text
-                break;
-            case "π":
-                currentText += Math.PI; // Append Pi to current text
-                break;
-            case "%":
-                currentText += "/100"; // Append division by 100 to current text
-                break;
-            case "+/-":
-                // Handle change of sign
-                if (!currentText.isEmpty()) {
-                    String[] parts = currentText.split("[+\\-*/]");
-                    String lastNumber = parts[parts.length - 1];
-                    if (!lastNumber.isEmpty() && !lastNumber.equals(".")) {
-                        if (lastNumber.charAt(0) == '-') {
-                            currentText = currentText.substring(0, currentText.length() - lastNumber.length()) + lastNumber.substring(1);
-                        } else {
-                            currentText = currentText.substring(0, currentText.length() - lastNumber.length()) + "-" + lastNumber;
+        try {
+            switch (command) {
+                // Handle different button actions
+                case "=":
+                    // Evaluate expression
+                    currentText  = currentText.replace("×", "*");
+                    currentText = currentText.replace("÷", "/");
+                    double result = calc.eval(currentText);
+                    if (result % 1 == 0) {
+                        int int_result = (int) result;
+                        currentText = int_result + "";
+                    } else {
+                        currentText = result + "";
+                    }
+                    break;
+                case "c":
+                    currentText = ""; // Clear current text
+                    break;
+                case "e":
+                    currentText += Math.E; // Append Euler's number to current text
+                    break;
+                case "π":
+                    currentText += Math.PI; // Append Pi to current text
+                    break;
+                case "%":
+                    currentText += "/100"; // Append division by 100 to current text
+                    equals();
+                    break;
+                case "+/-":
+                    // Handle change of sign
+                    if (!currentText.isEmpty()) {
+                        String[] parts = currentText.split("[+\\-*/]");
+                        String lastNumber = parts[parts.length - 1];
+                        if (!lastNumber.isEmpty() && !lastNumber.equals(".")) {
+                            if (lastNumber.charAt(0) == '-') {
+                                currentText = currentText.substring(0, currentText.length() - lastNumber.length()) + lastNumber.substring(1);
+                            } else {
+                                currentText = currentText.substring(0, currentText.length() - lastNumber.length()) + "-" + lastNumber;
+                            }
                         }
                     }
-                }
-                break;
-            case "10ˣ":
-                currentText += "*10^"; // Append exponentiation expression to current text
-                break;
-            case "sin":
-                currentText = "sin(" + currentText + ")"; // Append sine function to current text
-                break;
-            case "cos":
-                currentText = "cos(" + currentText + ")"; // Append cosine function to current text
-                break;
-            case "tan":
-                currentText = "tan(" + currentText + ")"; // Append tan function to current text
-                break;
+                    break;
+                case "10ˣ":
+                    currentText += "*10^"; // Append exponentiation expression to current text
+
+                    break;
+                case "sin":
+                    currentText = "sin(" + currentText + ")";// Append sine function to current text
+                    equals();
+                    break;
+                case "cos":
+                    currentText = "cos(" + currentText + ")"; // Append cosine function to current text
+                    equals();
+                    break;
+                case "tan":
+                    currentText = "tan(" + currentText + ")"; // Append tan function to current text
+                    equals();
+                    break;
                 case "sin⁻¹":
-                currentText = "asin(" + currentText + ")"; // Append arcsine function to current text
-                break;
-            case "cos⁻¹":
-                currentText = "acos(" + currentText + ")";   // Append arccosine function to current text
-                break;
-            case "tan⁻¹":
-                
-                currentText = "atan(" + currentText + ")";   // Appendarctan function to current text
-                break;
-            case "1/x":
-                currentText = "1/(" + currentText + ")";     // Prepend division by x to the current text
-                break;
-            case "EE":
-                currentText += "E";        // Append Euler's number to the current text
-                break;
-            case "log₁₀":
-                currentText = "log10(" + currentText + ")";  // Append logarithm base 10 to the current text
-                break;
-            case "x²":
-                currentText = "(" + currentText + ")^2";          // Square the value of the last number in the equation
-                break;
-            case "x³":
-                currentText = "(" + currentText + ")^3";          // Raise current text to power of 3
-                break;
-            case " xʸ":
-                currentText += "^";        // Add a multiplication sign before raising to power
-                break;
-            case "²√x":
-                currentText = "sqrt(" + currentText + ")";             // Take square root of current text
-                break;
-            case "∛x":
-                currentText = "cbrt(" + currentText + ")";               // Take cube root of current text
-                break;
-            case "ʸ√ₓ":
-                currentText += "√";        // Add a square root symbol after multiplying by it
-                break;
-                case "X!": 
-                // Add factorial logic
-                if (!currentText.isEmpty()) {
-                    // Split currentText by operators to get the last number
-                    String[] parts = currentText.split("[+\\-*/]");
-                    String lastNumber = parts[parts.length - 1];
-                    // Calculate factorial of the last number
-                    int number = Integer.parseInt(lastNumber);
-                    long factorial = 1;
-                    for (int i = 1; i <= number; i++) {
-                        factorial *= i;
+                    currentText = "asin(" + currentText + ")"; // Append arcsine function to current text
+                    equals();
+                    break;
+                case "cos⁻¹":
+                    currentText = "acos(" + currentText + ")";   // Append arccosine function to current text
+                    equals();
+                    break;
+                case "tan⁻¹":
+                    currentText = "atan(" + currentText + ")";   // Appendarctan function to current text
+                    equals();
+                    break;
+                case "1/x":
+                    currentText = "1/(" + currentText + ")";     // Prepend division by x to the current text
+                    equals();
+                    break;
+                case "EE":
+                    currentText += "E";        // Append Euler's number to the current text
+                    break;
+                case "log₁₀":
+                    currentText = "log10(" + currentText + ")";  // Append logarithm base 10 to the current text
+                    equals();
+                    break;
+                case "x²":
+                    currentText = "(" + currentText + ")^2";// Square the value of the last number in the equation
+                    equals();
+                    break;
+                case "x³":
+                    currentText = "(" + currentText + ")^3";          // Raise current text to power of 3
+                    equals();
+                    break;
+                case " xʸ":
+                    currentText += "^";        // Add a multiplication sign before raising to power
+                    break;
+                case "²√x":
+                    currentText = "sqrt(" + currentText + ")";// Take square root of current text
+                    equals();
+                    break;
+                case "∛x":
+                    currentText = "cbrt(" + currentText + ")";               // Take cube root of current text
+                    equals();
+                    break;
+                case "ʸ√ₓ":
+                    String root = JOptionPane.showInputDialog("Enter the root (y):");
+                    if (root != null && !root.isEmpty()) {
+                        double y = Double.parseDouble(root);
+                        double x = Double.parseDouble(currentText);
+                        double results = Math.pow(x, 1 / y);
+                        currentText = Double.toString(results);
                     }
-                    // Replace the last number with its factorial
-                    currentText = currentText.substring(0, currentText.length() - lastNumber.length()) + factorial;
-                }
+                    equals();
+                    break;
+                case "X!":
+                    // Add factorial logic
+                    if (!currentText.isEmpty()) {
+                        // Split currentText by operators to get the last number
+                        String[] parts = currentText.split("[+\\-*/]");
+                        String lastNumber = parts[parts.length - 1];
+                        // Calculate factorial of the last number
+                        int number = Integer.parseInt(lastNumber);
+                        long factorial = 1;
+                        for (int i = 1; i <= number; i++) {
+                            factorial *= i;
+                        }
+                        // Replace the last number with its factorial
+                        currentText = currentText.substring(0, currentText.length() - lastNumber.length()) + factorial;
+                    }
+                    equals();
+                    break;
+                case "Rad":
+                    double value = Double.parseDouble(currentText);
+                    currentText = String.valueOf(Math.toRadians(value));
+                    break;
+                case "mc":
+                    currentText = "";
+                    break;
+                case "mr":
+                     // Recall the value from memory
+                    currentText.equals(getMemory());
+                    break;
+                case "m+":
+                    currentText.equals(addToMemory()); // Add current value to memory
+                    currentText = ""; // Clear the current text
+                    break;
+                case "m-":
+                    currentText.equals(subtractFromMemory()); // Subtract current value from memory
+                    currentText = ""; // Clear the current text
+                    break;
+                case "Rand":
+                    currentText = String.valueOf(Math.random());
+                    currentText.equals(currentText.substring(0,currentText.length()-4));
                 break;
-            case "Rad":
-                double value = Double.parseDouble(currentText);
-                currentText = String.valueOf(Math.toRadians(value));
-                break;
-            // Add cases for other buttons as needed
-            default:
-                // Append the command to current text
-                if (currentText.isEmpty() && command.equals("(")) {
-                    currentText += "(";
-                } else if (!currentText.isEmpty() && currentText.charAt(currentText.length() - 1) == ')' && "+-×÷".contains(command)) {
-                    currentText += command;
-                } else if (!currentText.isEmpty() && "+-×÷".contains(currentText.charAt(currentText.length() - 1) + "") && command.equals("(")) {
-                    currentText += "(";
-                } else {
-                    currentText +=  command;
-                }
-                break;
+                case "eˣ":
+                    double exp= Math.E;
+                    currentText += ("*"+exp+"^");
+
+                    break;
+                // Add cases for other buttons as needed
+                default:
+                    // Append the command to current text
+                    if (currentText.isEmpty() && command.equals("(")) {
+                        currentText += "(";
+                    } else if (!currentText.isEmpty() && currentText.charAt(currentText.length() - 1) == ')' && "+-×÷".contains(command)) {
+                        currentText += command;
+                    } else if (!currentText.isEmpty() && "+-×÷".contains(currentText.charAt(currentText.length() - 1) + "") && command.equals("(")) {
+                        currentText += "(";
+                    } else {
+                        currentText +=  command;
+                    }
+                    break;
+            }
+        } catch (Exception ex) {
+            currentText = "Error";
         }
         screen.setText(currentText); // Update the text field with the current text
     }
